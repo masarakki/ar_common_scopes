58ef6b21da3dfd7b733948e1bf3714fc96c83e74
Yamada Masaki <masaki@hisme.net>
2009-05-25 10:52:05 +0900

    :allを2.2.2でも動くように
    どうやら2.3.2で動いてたのは偶然だったようだ

--- lib/ar_common_scopes.rb
+++ lib/ar_common_scopes.rb
@@ -1,6 +1,6 @@
 # ArCommonScopes
 class ActiveRecord::Base
-    named_scope :all, :all
+    named_scope :all, {}
     named_scope :limit, lambda{|limit| {:limit => limit}}
     named_scope :page, lambda{|page, size| {:limit => size, :offset => page * size}}
     
